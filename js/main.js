62771dae093f541bec25d34940b892cadd8c6964
Andrew Gustafson <tandrewgus@gmail.com>
2020-09-12 10:51:09 -0400

    Updated to use css-grid for layout

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 function toggleNav() {
 	var element = document.getElementById("nav");
 	element.classList.toggle("revealTOC");
+	var element = document.getElementById("header");
+	element.classList.toggle("bg-add");
 }
